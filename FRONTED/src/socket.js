9a3eac1700d899a2f9b2516208b8b79acfe79499
codllm <nishantnikhil360@gmail.com>
2026-03-29 12:19:01 +0530

    socket link fix

--- FRONTED/src/socket.js
+++ FRONTED/src/socket.js
@@ -1,18 +1,9 @@
 import { io } from 'socket.io-client';
 
+const URL = import.meta.env.VITE_BASE_URL;
 
-const URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3060';
-
-export const socket = io(URL, {
-    transports: ['websocket'],
-    autoConnect: true,
+const socket = io(URL, {
+    transports: ['websocket']
 });
 
-
-socket.on('connect', () => {
-    console.log('Connected to backend at:', URL);
-});
-
-socket.on('connect_error', (err) => {
-    console.log('Socket Connection Error:', err.message);
-});
+export default socket;
